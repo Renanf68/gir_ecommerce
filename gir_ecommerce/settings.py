f93a6f4285917cf99f5d65bbb6b9ffff6c8ca66b
Renan Costa <renancostam@gmail.com>
2016-08-09 18:12:37 -0300

    ajustando whitenoise no projeto

--- gir_ecommerce/settings.py
+++ gir_ecommerce/settings.py
@@ -39,10 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'catalog',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -64,6 +66,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                #app
+                'catalog.context_processors.categories',
             ],
         },
     },
