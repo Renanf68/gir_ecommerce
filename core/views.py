0dd08eb314b113675a3d6981f135baf4a6526a84
Renan Costa <renancostam@gmail.com>
2016-08-09 20:35:31 -0300

    ajustando requerements

--- core/views.py
+++ core/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .forms import ContactForm
+
 def index(request):
 	return render(request, 'index.html')
 
 def contact(request):
-	return render(request, 'contact.html')
+	form = ContactForm()
+	context = {
+		'form': form
+	}
+	return render(request, 'contact.html', context)
 
